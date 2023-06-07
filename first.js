3eb3c8818cf8f2f119868f68c8c55fe0b99d94eb
drbenasmah <drbenasmah2016@gmail.com>
2023-06-07 21:37:30 +0200

    update

--- first.js
+++ first.js
@@ -1,5 +1,12 @@
-let data = 8 
-let user = "Ben"
-let num1 = 7.8  
-console.log(typeof num1)
-console.log(typeof user)
+//let data = 8 
+//let user = "Ben"
+//let num1 = 7.8  
+//console.log(typeof num1)
+//console.log(typeof user)
+
+let i = 1; 
+while(i<=20)
+{
+  console.log("Hi");
+  i++;
+}
